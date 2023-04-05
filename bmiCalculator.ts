b4a02097c0e676b3e8b6c47910224331773a33dc
Ashton Bennett <ashtonbennett@ashtons-imac.lan>
2023-04-04 20:22:07 -0600

    continued exercises 1-4

--- bmiCalculator.ts
+++ bmiCalculator.ts
@@ -1,5 +1,3 @@
-// BMI, basic categories
-// Category	BMI (kg/m2)[c]	BMI Prime[c]
 // Underweight (Severe thinness)	< 16.0
 // Underweight (Moderate thinness)	16.0 – 16.9
 // Underweight (Mild thinness)	17.0 – 18.4
@@ -9,30 +7,54 @@
 // Obese (Class II)	35.0 – 39.9
 // Obese (Class III)	≥ 40.0
 
-const calculateBmi = (height: number, weight: number): string => {
-  const currentBmi: number = (weight / Math.pow(height, 2)) * 10_000;
+const parseArguments = (args: string[]) => {
+  if (args.length < 4) throw new Error("Not enough arguments");
+  if (args.length > 4) throw new Error("Too many arguments");
 
-  switch (true) {
-    case currentBmi <= 16:
-      return "Underweight (Severe thinness)";
-    case currentBmi >= 16 && currentBmi <= 16.9:
-      return "Underweight (Moderate thinness)";
-    case currentBmi >= 17.0 && currentBmi <= 18.4:
-      return "Underweight(Mild thinness)";
-    case currentBmi >= 18.5 && currentBmi <= 24.9:
-      return "Normal (healthy weight)";
-    case currentBmi >= 25.0 && currentBmi <= 29.9:
-      return "Overweight (Pre-obese)";
-    case currentBmi >= 30 && currentBmi <= 34.9:
-      return "Obese (Class I)";
-    case currentBmi >= 35 && currentBmi <= 39.9:
-      return "Obese (Class II)";
-    case currentBmi >= 40.0:
-      return "Obese (Class III)";
-    default:
-      "Unable to calculate";
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return true;
+  } else {
+    throw new Error("Provided values were not numbers!");
   }
-
-  return "function returned";
 };
-console.log(calculateBmi(180, 74));
+
+const calculateBmi = (height: number, weight: number): string => {
+  if (parseArguments(process.argv)) {
+    try {
+      const currentBmi: number = (weight / Math.pow(height, 2)) * 10_000;
+
+      switch (true) {
+        case currentBmi <= 16:
+          return "Underweight (Severe thinness)";
+        case currentBmi >= 16 && currentBmi <= 16.9:
+          return "Underweight (Moderate thinness)";
+        case currentBmi >= 17.0 && currentBmi <= 18.4:
+          return "Underweight(Mild thinness)";
+        case currentBmi >= 18.5 && currentBmi <= 24.9:
+          return "Normal (healthy weight)";
+        case currentBmi >= 25.0 && currentBmi <= 29.9:
+          return "Overweight (Pre-obese)";
+        case currentBmi >= 30 && currentBmi <= 34.9:
+          return "Obese (Class I)";
+        case currentBmi >= 35 && currentBmi <= 39.9:
+          return "Obese (Class II)";
+        case currentBmi >= 40.0:
+          return "Obese (Class III)";
+        default:
+          "Unable to calculate";
+      }
+
+      return "function returned";
+    } catch (error: unknown) {
+      let errorMessage = "Something bad happened.";
+      if (error instanceof Error) {
+        errorMessage += " Error: " + error.message;
+      }
+      console.log(errorMessage);
+    }
+  }
+};
+
+const a: number = Number(process.argv[2]);
+const b: number = Number(process.argv[3]);
+console.log(calculateBmi(a, b));
--- exerciseCalculator.ts
+++ exerciseCalculator.ts
@@ -3,7 +3,6 @@ const trainingDaysCalc = (sessions: number[]) => {
   for (let i = 0; i < 8; i++) {
     if (sessions[i] > 0) {
       numOfTrainingDays += 1;
-      console.log("lopped");
     }
   }
   return numOfTrainingDays;
@@ -20,22 +19,39 @@ const ratingCalc = (average: number, target: number): [number, string] => {
   }
 };
 
-const calculateExercises = (sessions: number[], target: number) => {
-  const numOfDays: number = 7;
-  const trainingDays: number = trainingDaysCalc(sessions);
-  const average: number = sessions.reduce((a, b) => a + b) / 7;
-  const success: boolean = average >= target;
-  const rating: [number, string] = ratingCalc(average, target);
+interface Result {
+  periodLength: number;
+  trainingDays: number;
+  success: boolean;
+  rating: number;
+  ratingDescription: string;
+  target: number;
+  average: number;
+}
+
+const calculateExercises = (sessions: number[], target: number): Result => {
+  const average = sessions.reduce((a, b) => a + b) / sessions.length;
+  const rating = ratingCalc(average, target);
 
   return {
-    periodLength: numOfDays,
-    trainingDays: trainingDays,
-    success: success,
+    periodLength: sessions.length,
+    trainingDays: trainingDaysCalc(sessions),
+    average: average,
+    success: average >= target,
     rating: rating[0],
     ratingDescription: rating[1],
     target: target,
-    average: average,
   };
 };
 
-console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2));
+const findInputTimes = () => {
+  const inputGiven: number[] = [];
+  for (let i = 2; i < process.argv.length; i++) {
+    inputGiven.push(parseInt(process.argv[i]));
+  }
+  return inputGiven;
+};
+
+const exerciseTimes: number[] = findInputTimes();
+const exerciseGoal: number[] = exerciseTimes.slice(-1); //try .at(-1)
+console.log(calculateExercises(exerciseTimes, exerciseGoal));
